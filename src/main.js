3187c172f15bcc1094c2fd04f91203bc0aafd0b7
Alexandre Salvador de Oliveira <alexandresalvadoroliveiradev@gmail.com>
2020-06-10 20:23:03 -0300

    Refresh containers is avaiable now

--- src/main.js
+++ src/main.js
@@ -7,74 +7,76 @@ const execPromise = promisify(exec);
 
 app.dock.hide();
 
-async function findContainers() {
-  const command = await execPromise(
-    'docker ps -a --format "{{.ID}} {{.Names}}"',
-  );
-  const containers = command.stdout
-    .split(/\n/g)
-    .filter(item => item.length > 0);
-  const containersMenu = [];
-  containers.forEach(item => {
-    const [containerId, containerName] = item.split(' ');
-    const menu = new MenuItem({
-      id: containerId,
-      label: containerName,
-      submenu: [
-        {
-          id: containerId,
-          label: 'Start',
-          click: async function (menuItem, browserWindow, event) {
-            const commandIsStopped = `docker ps -a --filter "id=${menuItem.id}" --filter status=exited --format "{{.ID}}"`;
-            const isStopedContainerId = await execPromise(commandIsStopped);
-            if (isStopedContainerId) {
-              const id = isStopedContainerId.stdout.replace(/\n/g, '');
-              const commandStart = `docker start ${id}`;
-              await execPromise(commandStart);
-            }
-          },
-        },
-        {
-          id: containerId,
-          label: 'Stop',
-          click: async function (menuItem, browserWindow, event) {
-            const commandIsRunning = `docker ps -a --filter "id=${menuItem.id}" --filter status=running --format "{{.ID}}"`;
-            const isRunningContainerId = await execPromise(commandIsRunning);
-            if (isRunningContainerId) {
-              const id = isRunningContainerId.stdout.replace(/\n/g, '');
-              const commandStart = `docker stop ${id}`;
-              await execPromise(commandStart);
-            }
-          },
-        },
-      ],
-    });
-    containersMenu.push(menu);
-  });
-  return containersMenu;
-}
-
-function renderMenu(containers) {
-  const refreshItem = new MenuItem({
-    id: 'refresh-item',
-    label: 'Refresh',
-    click: function (menuItem, browserWindow, event) {
-      const containers = findContainers();
-      renderMenu(containers);
-    },
-  });
-
-  const menu = Menu.buildFromTemplate([
-    refreshItem,
-    new MenuItem({ type: 'separator' }),
-    ...containers,
-  ]);
-  return menu;
-}
-
 app.whenReady().then(async () => {
   const controltainerIcon = resolve(__dirname, 'assets', 'icon', 'icon.png');
   let tray = new Tray(controltainerIcon);
+
+  async function findContainers() {
+    const command = await execPromise(
+      'docker ps -a --format "{{.ID}} {{.Names}}"',
+    );
+    const containers = command.stdout
+      .split(/\n/g)
+      .filter(item => item.length > 0);
+    const containersMenu = [];
+    containers.forEach(item => {
+      const [containerId, containerName] = item.split(' ');
+      const menu = new MenuItem({
+        id: containerId,
+        label: containerName,
+        submenu: [
+          {
+            id: containerId,
+            label: 'Start',
+            click: async function (menuItem, browserWindow, event) {
+              const commandIsStopped = `docker ps -a --filter "id=${menuItem.id}" --filter status=exited --format "{{.ID}}"`;
+              const isStopedContainerId = await execPromise(commandIsStopped);
+              if (isStopedContainerId) {
+                const id = isStopedContainerId.stdout.replace(/\n/g, '');
+                const commandStart = `docker start ${id}`;
+                await execPromise(commandStart);
+              }
+            },
+          },
+          {
+            id: containerId,
+            label: 'Stop',
+            click: async function (menuItem, browserWindow, event) {
+              const commandIsRunning = `docker ps -a --filter "id=${menuItem.id}" --filter status=running --format "{{.ID}}"`;
+              const isRunningContainerId = await execPromise(commandIsRunning);
+              if (isRunningContainerId) {
+                const id = isRunningContainerId.stdout.replace(/\n/g, '');
+                const commandStart = `docker stop ${id}`;
+                await execPromise(commandStart);
+              }
+            },
+          },
+        ],
+      });
+      containersMenu.push(menu);
+    });
+    return containersMenu;
+  }
+
+  function renderMenu(containers) {
+    const refreshItem = new MenuItem({
+      id: 'refresh-item',
+      label: 'Refresh',
+      click: async function (menuItem, browserWindow, event) {
+        const containers = await findContainers();
+        const contextMenu = renderMenu(containers);
+        tray.setContextMenu(contextMenu);
+      },
+    });
+
+    const menu = Menu.buildFromTemplate([
+      refreshItem,
+      new MenuItem({ type: 'separator' }),
+      ...containers,
+    ]);
+    return menu;
+  }
+
   const containers = await findContainers();
   const contextMenu = renderMenu(containers);
   tray.setToolTip('Controlling you containers is simple.');
